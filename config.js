183c555df7381efdfe0be55c97443dd35a358580
FD LORD / FAMOUS-TECH <167099698+Azorwolldens@users.noreply.github.com>
2024-07-16 20:35:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   prefix: '.', // Vous pouvez changer le préfixe ici
   apiKeyOpenAI: 'your_openai_api_key_here', // Remplacez par votre clé API OpenAI
   telegramBotToken: 'your_telegram_bot_token_here', // Remplacez par votre token de bot Telegram
-  pixabayApiKey: 'your_pixabay_api_key_here', // Remplacez par votre clé API Pixabay
+  pixabayApiKey: '*36545097-d5df6c20dfd41fe6ace3f8fa', // Remplacez par votre clé API Pixabay
   youtubeApiKey: 'your_youtube_api_key_here', // Remplacez par votre clé API YouTube
   weatherApiKey: 'your_weather_api_key_here', // Remplacez par votre clé API Weather
   gitRepoUrl: 'https://github.com/Azorwolldens/FAMOUS-TG', // Remplacez par l'URL de votre dépôt GitHub
