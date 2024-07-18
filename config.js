e5912999ad89ef70b20787ec67bc01d1cb2d367e
FD LORD / FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-07-18 14:35:04 -0400

    V2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   prefix: '/', // Vous pouvez changer le préfixe ici
   apiKeyOpenAI: 'your_openai_api_key_here', // Remplacez par votre clé API OpenAI
-  telegramBotToken: 'your_telegram_bot_token_here', // Remplacez par votre token de bot Telegram
+  telegramBotToken: '7487424477:AAHZDF2vYBKNiKdEfmbJ-PUrxsZGAIdKtBI', // Remplacez par votre token de bot Telegram
   pixabayApiKey: '*36545097-d5df6c20dfd41fe6ace3f8fa', // Remplacez par votre clé API Pixabay
   youtubeApiKey: 'AIzaSyD-5uKeppXCwIl5opur66UI1FMC82ygGws', // Remplacez par votre clé API YouTube
   weatherApiKey: 'your_weather_api_key_here', // Remplacez par votre clé API Weather
