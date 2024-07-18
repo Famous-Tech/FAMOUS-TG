87c9623dba096d82127b6358ea60b0fc3370c2a2
FD LORD / FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-07-18 12:59:28 -0400

    V2

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
   pixabayApiKey: '*36545097-d5df6c20dfd41fe6ace3f8fa', // Remplacez par votre clé API Pixabay
   youtubeApiKey: 'AIzaSyD-5uKeppXCwIl5opur66UI1FMC82ygGws', // Remplacez par votre clé API YouTube
   weatherApiKey: 'your_weather_api_key_here', // Remplacez par votre clé API Weather
-  gitRepoUrl: 'https://github.com/Azorwolldens/FAMOUS-TG', // Remplacez par l'URL de votre dépôt GitHub
+  gitRepoUrl: 'https://github.com/Famous-Tech/FAMOUS-TG', // Remplacez par l'URL de votre dépôt GitHub
   ownerWhatsAppNumber: '50943782508' // Remplacez par votre numéro WhatsApp
 };
