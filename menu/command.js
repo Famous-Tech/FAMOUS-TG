8da43823424e6b0462a323d5b6340fc05fdbc78f
FD LORD / FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-07-21 13:46:30 -0400

    V3

---
+++ menu/command.js
@@ -0,0 +1,25 @@
+const commands = [
+  '/start',
+  '/help',
+  '/gpt',
+  '/list',
+  '/alive',
+  '/ping',
+  '/repo',
+  '/uptime',
+  '/menu',
+  '/img',
+  '/song',
+  '/url',
+  '/morse',
+  '/dev',
+  '/antilink',
+  '/warn',
+  '/kick',
+  '/calc',
+  '/tagall',
+  '/convert',
+  '/admin'
+];
+
+module.exports = commands;
