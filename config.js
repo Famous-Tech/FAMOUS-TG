a95d416588ce7676f3b1c38359f787ddbf89d9e2
FD LORD / FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-07-23 11:24:19 -0400

    Last

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  prefix: '/', // Vous pouvez changer le préfixe ici
-  apiKeyOpenAI: 'your_openai_api_key_here', // Remplacez par votre clé API OpenAI
-  telegramBotToken: '7487424477:AAHZDF2vYBKNiKdEfmbJ-PUrxsZGAIdKtBI', // Remplacez par votre token de bot Telegram
-  pixabayApiKey: '*36545097-d5df6c20dfd41fe6ace3f8fa', // Remplacez par votre clé API Pixabay
-  youtubeApiKey: 'AIzaSyD-5uKeppXCwIl5opur66UI1FMC82ygGws', // Remplacez par votre clé API YouTube
-  weatherApiKey: 'your_weather_api_key_here', // Remplacez par votre clé API Weather
-  gitRepoUrl: 'https://github.com/Famous-Tech/FAMOUS-TG', // Remplacez par l'URL de votre dépôt GitHub
-  ownerWhatsAppNumber: '50943782508' // Remplacez par votre numéro WhatsApp
+  prefix: process.env.PREFIX, // Préfixe
+  apiKeyOpenAI: process.env.OPENAI_API_KEY, // Clé API OpenAI
+  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN, // Token du bot Telegram
+  pixabayApiKey: process.env.PIXABAY_API_KEY, // Clé API Pixabay
+  youtubeApiKey: process.env.YOUTUBE_API_KEY, // Clé API YouTube
+  gitRepoUrl: process.env.GIT_REPO_URL, // URL du dépôt GitHub
+  ownerWhatsAppNumber: process.env.OWNER_WHATSAPP_NUMBER // Numéro WhatsApp
 };
